de733d2a3edab6f87b7efe3935314364a89ec1d2
Carl Almodovar <pnutbuttah123@gmail.com>
2017-11-10 02:09:25 +0000

    My First Commit

---
+++ object.js
@@ -0,0 +1,29 @@
+
+
+
+var hotels = {
+    name: 'Biltmore',
+    rooms: 500,
+    roomsBooked: 150,
+    roomService: true,
+    pool: false,
+    
+    roomsAvail: function() {
+        return this.rooms - this.roomsBooked;
+        
+        
+        
+    }
+};
+
+document.getElementById('mybutton').onclick = function() {
+document.getElementById('hotelName').innerHTML = hotels.name;
+document.getElementById('availableRooms').innerHTML = hotels.roomsAvail();
+document.getElementById('totalRooms').innerHTML = hotels.rooms;
+
+}
+
+
+
+
+
